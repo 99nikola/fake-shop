09bcc6634d9268928bf976e4b15e6157ed62675a
Nikola <nikola.popovicc99@gmail.com>
2021-12-09 15:37:11 +0100

    types for LoginForm ad login page

---
+++ pages/login.tsx
@@ -0,0 +1,25 @@
+import { NextPage } from "next";
+import Flex from "../components/atoms/Flex.styled";
+import LoginForm from "../components/organisms/LoginForm";
+import { IUser } from "../typescript/interfaces/Users";
+
+const Login: NextPage = () => {
+
+    const onLogin= (user: IUser) => {
+        console.log(user);
+    }
+
+
+    return (
+        <Flex
+            width="100%"
+            height="100%"
+            justifyContent="center"
+        >
+            <LoginForm 
+            />
+        </Flex>
+    );
+}
+
+export default Login;
