f8551eab187784750b57de3790ec0766756db943
Nikola <nikola.popovicc99@gmail.com>
2021-12-09 15:38:15 +0100

    styled navigation bar

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -1,17 +1,39 @@
 import Link from "next/link";
+import Flex from "../atoms/Flex.styled";
+import HeaderLink from "../atoms/HeaderLink";
 import Breadcrumb from "./Breadcrumb";
 
 const Navbar: React.FC = (props) => {
     return (
         <nav>
-            <Link href="/">Home</Link>
-            <Link href="/users">Users</Link>
-            <Link href="/products">Products</Link>
-            <Link href="/categories">Categories</Link>
-            <Link href="/carts">Carts</Link>
+            <Flex border="1px solid lightgray"
+                width="60%"
+                height="50px"
+                margin="0 0 0 20%"
+                justifyContent="space-evenly"
+                alignItems="center"
+            >   
+                <HeaderLink>
+                    <Link  href="/">Home</Link>
+                </HeaderLink>
+                <HeaderLink>
+                    <Link href="/users">Users</Link>
+                </HeaderLink>
+                <HeaderLink>
+                    <Link href="/products">Products</Link>
+                </HeaderLink>
+                <HeaderLink>
+                    <Link href="/categories">Categories</Link>
+                </HeaderLink>
 
+                <HeaderLink>
+                    <Link href="/carts">Carts</Link>
+                </HeaderLink>
+                <HeaderLink>
+                    <Link href="/login">Login</Link>
+                </HeaderLink>
+            </Flex>
             <Breadcrumb />
-
             <main>
                 {props.children}
             </main>
