7e6e190b8416d8f63c8d243e79bbc41eb7450cee
99nikola <89345205+99nikola@users.noreply.github.com>
2021-12-14 12:04:55 +0000

    Deploying to gh-pages from @ 99nikola/fake-shop@08c285324257d69492f0e2983239cae9e3200de4 🚀

---
+++ _next/static/Pbw-xW4aR54yb5mDGYBM0/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcategories\u002F[categoryName]\u002F[productId]","\u002Fcategories\u002F[categoryName]","\u002Fusers\u002F[userId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
