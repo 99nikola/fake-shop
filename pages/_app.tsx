566ba59cd61e3b3b5dfcbef53f6a601c7d8b04e6
Nikola <nikola.popovicc99@gmail.com>
2021-12-14 14:32:20 +0100

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,29 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import Navbar from "../components/layouts/Navbar"
 import { wrapper } from "../store";
+import { useEffect } from "react";
+import * as ga from "react-ga";
+import { useRouter } from "next/router";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = (url: any) => {
+            ga.pageview(url);
+        }
+        //When the component is mounted, subscribe to router changes
+        //and log those page views
+        router.events.on('routeChangeComplete', handleRouteChange)
+
+        // If the component is unmounted, unsubscribe
+        // from the event with the `off` method
+        return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events]);
+
 	return (
 		<Navbar>
 			<Component {...pageProps} />
